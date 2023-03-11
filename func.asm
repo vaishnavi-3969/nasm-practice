fc033f357f5aae656510a4d68aac725a98dab691
Vaishnavi Kale <vaishnavi.a.kale@cumminscollege.in>
2023-03-11 23:28:17 +0000

    function

---
+++ func.asm
@@ -0,0 +1,23 @@
+section	.text
+	global _start      
+_start:  
+    call func
+    call func
+    call func
+    mov eax,1
+    mov ebx,0
+    int 80h
+
+func:
+	mov ebp,esp
+	sub esp,2
+	mov [esp], byte 'H'
+	mov [esp+1], byte 'i'
+	mov eax,4
+	mov ebx,1
+	mov ecx,esp
+	mov edx,2
+	int 80h
+	mov esp,ebp
+	ret
+	
